b24d2a1807cbc68f2415bd73be94ec6ef6100613
James Farrell <farrej10@lg12l24.scss.tcd.ie>
2018-02-09 14:37:50 +0000

    Initial commit

---
+++ app/src/main/java/com/example/farrej10/quickbites/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.farrej10.quickbites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
