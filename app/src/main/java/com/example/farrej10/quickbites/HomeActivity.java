55c9afd0c23b3e881d98409601463952a17bfeb3
maherc7 <maherc7@tcd.ie>
2018-02-09 16:48:05 +0000

    added funtion for nearby button

--- app/src/main/java/com/example/farrej10/quickbites/HomeActivity.java
+++ app/src/main/java/com/example/farrej10/quickbites/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.farrej10.quickbites;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void startMap(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+
+
     }
 }
