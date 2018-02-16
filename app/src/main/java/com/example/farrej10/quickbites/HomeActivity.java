0660e20ce4b44543e8aeb8a0aaa2a8c5a75320fd
teoc <teoc@tcd.ie>
2018-02-16 15:31:20 +0000

    16 Feb 2018 - Lastest Update

--- app/src/main/java/com/example/farrej10/quickbites/HomeActivity.java
+++ app/src/main/java/com/example/farrej10/quickbites/HomeActivity.java
@@ -4,14 +4,18 @@ package com.example.farrej10.quickbites;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
@@ -27,10 +31,29 @@ public class HomeActivity extends AppCompatActivity {
         happyRate.setText(Integer.toString(burger.getRating()), TextView.BufferType.NORMAL);
     }
 
-    public void startMap(View view){
+    public void startMap(View view)
+    {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+    }
 
+    public void openDetailed(View view)
+    {
+        Intent restuarantDetailed = new Intent(this, detailedRestaurant.class);
+        TextView title = findViewById(R.id.Title);
+        TextView description = findViewById(R.id.description);
+        TextView ratingHappy = findViewById(R.id.happyNum);
+        TextView ratingNeutral = findViewById(R.id.neutralNum);
+        TextView ratingSad = findViewById(R.id.sadNum);
+
+        String restTitle = title.getText().toString();
+        String restDescription = description.getText().toString();
+        String restHappyR = ratingHappy.getText().toString();
+        String restNeuR = ratingNeutral.getText().toString();
+        String restSadR = ratingSad.getText().toString();
 
     }
+
+
+
 }
