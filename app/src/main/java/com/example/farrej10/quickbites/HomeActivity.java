518579b1514346d02c359fdf1fbc8a4624c4c301
teoc <teoc@tcd.ie>
2018-02-22 18:41:08 +0000

    Everything please save

--- app/src/main/java/com/example/farrej10/quickbites/HomeActivity.java
+++ app/src/main/java/com/example/farrej10/quickbites/HomeActivity.java
@@ -11,7 +11,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.sql.Types.NULL;
+
 public class HomeActivity extends AppCompatActivity {
+
+    Restaurant burger = new Restaurant("Burger Place ", "This Burger place is the home of the burger" , 10, 5, 1, 53.343247, -6.254476);
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,16 +26,66 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Restaurant burger = new Restaurant("Burger Place ", "This Burger place is the home of the burger" , 10);
+        List<Restaurant> listofRestaurants = new ArrayList<>();
+        csvParser parser = new csvParser();
+        parser.parseToList(listofRestaurants, this);
+
+
 
         TextView title = findViewById(R.id.Title);
-        title.setText(burger.getName(), TextView.BufferType.NORMAL);
+        title.setText(listofRestaurants.get(0).getName(), TextView.BufferType.NORMAL);
 
         TextView description = findViewById(R.id.description);
-        description.setText(burger.getDescipt(), TextView.BufferType.NORMAL);
+        description.setText(listofRestaurants.get(0).getDescrip(), TextView.BufferType.NORMAL);
 
         TextView happyRate = findViewById(R.id.happyNum);
-        happyRate.setText(Integer.toString(burger.getRating()), TextView.BufferType.NORMAL);
+        String Happyrating = Integer.toString(listofRestaurants.get(0).getRating(0));
+        happyRate.setText(Happyrating, TextView.BufferType.NORMAL);
+
+        TextView neutralRate = findViewById(R.id.neutralNum);
+        String Neurating = Integer.toString(listofRestaurants.get(0).getRating(1));
+        neutralRate.setText(Neurating, TextView.BufferType.NORMAL);
+
+        TextView sadRate = findViewById(R.id.sadNum);
+        String Sadrating = Integer.toString(listofRestaurants.get(0).getRating(2));
+        sadRate.setText(Sadrating, TextView.BufferType.NORMAL);
+
+        title = findViewById(R.id.Title2);
+        title.setText(listofRestaurants.get(1).getName(), TextView.BufferType.NORMAL);
+
+        description = findViewById(R.id.description2);
+        description.setText(listofRestaurants.get(1).getDescrip(), TextView.BufferType.NORMAL);
+
+        happyRate = findViewById(R.id.happyNum2);
+        Happyrating = Integer.toString(listofRestaurants.get(1).getRating(0));
+        happyRate.setText(Happyrating, TextView.BufferType.NORMAL);
+
+        neutralRate = findViewById(R.id.neutralNum2);
+        Neurating = Integer.toString(listofRestaurants.get(1).getRating(1));
+        neutralRate.setText(Neurating, TextView.BufferType.NORMAL);
+
+        sadRate = findViewById(R.id.sadNum2);
+        Sadrating = Integer.toString(listofRestaurants.get(1).getRating(2));
+        sadRate.setText(Sadrating, TextView.BufferType.NORMAL);
+
+        title = findViewById(R.id.Title3);
+        title.setText(listofRestaurants.get(2).getName(), TextView.BufferType.NORMAL);
+
+        description = findViewById(R.id.description3);
+        description.setText(listofRestaurants.get(2).getDescrip(), TextView.BufferType.NORMAL);
+
+        happyRate = findViewById(R.id.happyNum3);
+        Happyrating = Integer.toString(listofRestaurants.get(2).getRating(0));
+        happyRate.setText(Happyrating, TextView.BufferType.NORMAL);
+
+        neutralRate = findViewById(R.id.neutralNum3);
+        Neurating = Integer.toString(listofRestaurants.get(2).getRating(1));
+        neutralRate.setText(Neurating, TextView.BufferType.NORMAL);
+
+        sadRate = findViewById(R.id.sadNum3);
+        Sadrating = Integer.toString(listofRestaurants.get(2).getRating(2));
+        sadRate.setText(Sadrating, TextView.BufferType.NORMAL);
+
     }
 
     public void startMap(View view)
@@ -41,6 +98,7 @@ public class HomeActivity extends AppCompatActivity {
 
     public void openDetailed(View view)
     {
+
         Intent restuarantDetailed = new Intent(this, detailedRestaurant.class);
         TextView title = findViewById(R.id.Title2);
         TextView description = findViewById(R.id.description2);
@@ -50,9 +108,29 @@ public class HomeActivity extends AppCompatActivity {
 
         String restTitle = title.getText().toString();
         String restDescription = description.getText().toString();
-        String restHappyR = ratingHappy.getText().toString();
-        String restNeuR = ratingNeutral.getText().toString();
-        String restSadR = ratingSad.getText().toString();
+        String ratingString = ratingHappy.getText().toString();
+        int intHappyR = Integer.parseInt(ratingString);
+        if( intHappyR == NULL)
+        {
+            intHappyR = -1;
+        }
+        ratingString = ratingNeutral.getText().toString();
+        int intNeuR = Integer.parseInt(ratingString);
+        if( intNeuR == NULL)
+        {
+            intNeuR = -1;
+        }
+        ratingString = ratingSad.getText().toString();
+        int intSadR = Integer.parseInt(ratingString);
+        if( intSadR == NULL)
+        {
+            intSadR = -1;
+        }
+
+        Restaurant test = new Restaurant(restTitle, restDescription, intHappyR, intNeuR, intSadR, 53.343247, -6.254476);
+        restuarantDetailed.putExtra("rest", test);
+
+        startActivity(restuarantDetailed);
 
     }
 
--- app/src/main/java/com/example/farrej10/quickbites/Restaurant.java
+++ app/src/main/java/com/example/farrej10/quickbites/Restaurant.java
@@ -1,27 +1,54 @@
 package com.example.farrej10.quickbites;
 
+import java.io.Serializable;
+
 /**
  * Created by teoc on 09/02/18.
  */
 
-public class Restaurant {
+public class Restaurant implements Serializable{
     private String name;
-    private String descript;
-    private int    rating;
+    private String descrip;
+    private int    rating[] = new int[3];
+    private double Latitude;
+    private double Longitude;
 
     public String getName   () { return name; }
-    public String getDescipt () { return descript; }
-    public int    getRating () { return rating; }
+    public String getDescrip () { return descrip; }
+    public int    getRating (int index) { return rating[index]; }
+    public double getLatitude()  {return Latitude; }
+    public double getLongitude()  {return Longitude; }
     public void setName     (String n) { name = n; }
-    public void setDescip   (String d) { descript = d; }
-    public void setRating   (int r) { rating = r; }
+    public void setDescrip   (String d) { descrip = d; }
+    public void setRating   (int r, int index) { rating[index] = r; }
+    public void setLatitude (double Lat) { Latitude = Lat ; }
+    public void setLongitude(double Long) { Longitude = Long; }
 
-    public Restaurant(String n, String d, int r)
+    public Restaurant()
     {
-        name = n;
-        descript = d;
-        rating = r;
+        name = "";
+        descrip = "";
+        Latitude = 0.0;
+        Longitude = 0.0;
+        for (int i = 0; i < 3; i++)
+        {
+            rating[i] = 0;
+        }
 
     }
+
+    public Restaurant(String n, String d, int happy, int neutral, int sad, double Long, double Lat )
+    {
+        setName(n);
+        setDescrip(d);
+        setRating(happy, 0);
+        setRating(neutral, 1);
+        setRating(sad, 2);
+        setLatitude(Lat);
+        setLongitude(Long);
+
+    }
+
+
 
 }
