a398b65d4fe189c17cf38e9761b8b940bf906f63
calebteo <35841802+calebteo@users.noreply.github.com>
2018-02-22 16:48:29 +0000

    Revert "Detailed rest"

--- app/src/main/java/com/example/farrej10/quickbites/Restaurant.java
+++ app/src/main/java/com/example/farrej10/quickbites/Restaurant.java
@@ -1,54 +1,27 @@
 package com.example.farrej10.quickbites;
 
-import java.io.Serializable;
-
 /**
  * Created by teoc on 09/02/18.
  */
 
-public class Restaurant implements Serializable{
+public class Restaurant {
     private String name;
-    private String descrip;
-    private int    rating[] = new int[3];
-    private double Latitude;
-    private double Longitude;
+    private String descript;
+    private int    rating;
 
     public String getName   () { return name; }
-    public String getDescrip () { return descrip; }
-    public int    getRating (int index) { return rating[index]; }
-    public double getLatitude()  {return Latitude; }
-    public double getLongitude()  {return Longitude; }
+    public String getDescipt () { return descript; }
+    public int    getRating () { return rating; }
     public void setName     (String n) { name = n; }
-    public void setDescrip   (String d) { descrip = d; }
-    public void setRating   (int r, int index) { rating[index] = r; }
-    public void setLatitude (double Lat) { Latitude = Lat ; }
-    public void setLongitude(double Long) { Longitude = Long; }
+    public void setDescip   (String d) { descript = d; }
+    public void setRating   (int r) { rating = r; }
 
-    public Restaurant()
+    public Restaurant(String n, String d, int r)
     {
-        name = "";
-        descrip = "";
-        Latitude = 0.0;
-        Longitude = 0.0;
-        for (int i = 0; i < 3; i++)
-        {
-            rating[i] = 0;
-        }
+        name = n;
+        descript = d;
+        rating = r;
 
     }
-
-    public Restaurant(String n, String d, int happy, int neutral, int sad, double Long, double Lat )
-    {
-        setName(n);
-        setDescrip(d);
-        setRating(happy, 0);
-        setRating(neutral, 1);
-        setRating(sad, 2);
-        setLatitude(Lat);
-        setLongitude(Long);
-
-    }
-
-
 
 }
